096ddd0e68dd2b43fcdd2c4e05a0f5257dfdbc22
Jasdeep Singh <administrator@Administrators-MacBook-Pro-4.local>
2012-06-04 19:15:55 -0400

    Initial Commit - empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lcbobuddy::Application.config.secret_token = '30d88f6411c51b7f68e600fa2b4d5d53a6a5f2c1aed4deba48b37b1d7b57020442bf9c4aed1f39e075eca9dbb26825c48e2705635c386b7097cd1c7ee1d06939'
