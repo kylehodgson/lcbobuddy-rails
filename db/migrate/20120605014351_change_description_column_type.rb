9ec807002e6de107538948f8e16a70766ee74aff
Jasdeep Singh <narang.jasdeep@gmail.com>
2012-06-04 21:45:44 -0400

    change column type for description

---
+++ db/migrate/20120605014351_change_description_column_type.rb
@@ -0,0 +1,9 @@
+class ChangeDescriptionColumnType < ActiveRecord::Migration
+  def up
+  	change_column :products, :description, :text
+  end
+
+  def down
+  	change_column :products, :description, :string
+  end
+end
